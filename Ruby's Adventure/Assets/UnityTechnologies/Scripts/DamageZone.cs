23c6e923d2980ade37302a49c1e8bb31ef6067ad
Rocio Saavedra <rocio.saavedra@mymdc.net>
2023-11-12 09:19:11 -0500

    I submitted the actual project folder this time! (no build) or at least that's what I think I did. Hopefully this works!

---
+++ Ruby's Adventure/Assets/UnityTechnologies/Scripts/DamageZone.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// This class will apply continuous damage to the Player as long as it stay inside the trigger on the same object
+/// </summary>
+public class DamageZone2 : MonoBehaviour 
+{
+    void OnTriggerStay2D(Collider2D other)
+    {
+        RubyController2 controller = other.GetComponent<RubyController2>();
+
+        if (controller != null)
+        {
+            //the controller will take care of ignoring the damage during the invincibility time.
+            controller.ChangeHealth(-1);
+        }
+    }
+}
---
+++ Ruby's Adventure/Assets/UnityTechnologies/Scripts/HealthCollectible.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Will handle giving health to the character when they enter the trigger.
+/// </summary>
+public class HealthCollectible2 : MonoBehaviour 
+{
+    public AudioClip collectedClip;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        RubyController2 controller = other.GetComponent<RubyController2>();
+
+        if (controller != null)
+        {
+            if (controller.health < controller.maxHealth)
+            {
+                controller.ChangeHealth(1);
+                Destroy(gameObject);
+
+                controller.PlaySound(collectedClip);
+            }
+        }
+    }
+}
